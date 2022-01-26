ef8354d07129c337caf19f58f111c5a76321c2b1
border <border@student.21-school.ru>
2022-01-26 20:05:44 +0300

    set, map готовы

--- main.cpp
+++ main.cpp
@@ -1,17 +1,78 @@
-#include <iostream>
+#include "map.hpp"
+#include "iterator.hpp"
+#include "set.hpp"
 #include "stack.hpp"
-#include <stack>
+#include "utils.hpp"
+#include "vector.hpp"
+#include <map>
 #include <vector>
+#include <stack>
+#include <set>
+
+
+template <typename T>
+void println(std::vector<T> a)
+{
+	std::vector<int>::iterator it = a.begin();
+	std::cout  << "Vector:" << std::endl;
+	while(it != a.end()) {
+		std::cout << *it << " ";
+		it++;
+	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+void println(ft::vector<T> a)
+{
+	ft::vector<int>::iterator it = a.begin();
+	std::cout << "My vector:" << std::endl;
+	while(it != a.end()) {
+		std::cout << *it << " ";
+		it++;
+	}
+	std::cout << std::endl;
+}
 
 int main() {
+
+	//vector
 	std::vector<int> vector;
-	std::vector<int> myVec;
+	ft::vector<int> myVec;
+	std::cout << "Vector size: " << vector.size() << std::endl;
+	std::cout << "My vector size: " << myVec.size() << std::endl;
+
+	std::vector<int> vector2 (10,20);
+	println(vector2);
+	ft::vector<int> myVec2 (10,20);
+	println(myVec2);
+
+	vector2.erase(vector2.end() - 1);
+	println(vector2);
+	myVec2.erase(myVec2.end() - 1);
+	println(myVec2);
+
+	std::cout << "Vector size: " << vector2.size() << " " << vector2.capacity() << std::endl;
+	vector2.push_back(3);
+	println(vector2);
+	std::cout << "My vector size: " << myVec2.size() << " " << myVec2.capacity() << std::endl;
+	myVec2.push_back(3);
+	println(myVec2);
+
+	std::cout << "Vector size: " << vector2.size() << std::endl;
+	vector2.resize(15);
+	std::cout << "Vector size: " << vector2.size() << " " << vector2.capacity() << std::endl;
+
+	std::cout << "My vector size: " << myVec2.size() << std::endl;
+	myVec2.resize(15);
+	std::cout << "My vector size: " << myVec2.size() << " " << vector2.capacity() << std::endl;
 
 
+	//stack
 	ft::stack<int> first;
 	ft::stack<int> second (myVec);
-	ft::stack<int,std::vector<int> > third;
-	ft::stack<int,std::vector<int> > six (myVec);
+	ft::stack<int,ft::vector<int> > third;
+	ft::stack<int,ft::vector<int> > six (myVec2);
 
 	std::cout << "size of first: " << first.size() << '\n';
 	std::cout << "size of second: " << second.size() << '\n';
@@ -29,5 +90,186 @@ int main() {
 	if (first> six) std::cout << "first is greater than six\n";
 	if (first<=six) std::cout << "first is less than or equal to six\n";
 	if (first>=six) std::cout << "first is greater than or equal to six\n";
+
+	//map
+	std::map<int, int> map2;
+	map2[1] = 1;
+	map2[2] = 2;
+	map2[3] = 3;
+	std::map<int, int>::iterator mapIt = map2.begin();
+	std::cout << "Map:" << std::endl;
+	while (mapIt != map2.end()) {
+		std::cout << mapIt->second << " ";
+		mapIt++;
+	}
+	ft::map<int, int> myMap;
+	myMap[1] = 1;
+	myMap[2] = 2;
+	myMap[3] = 3;
+	ft::map<int, int>::iterator myMapIt = myMap.begin();
+	std::cout << " " << std::endl << "My map:" << std::endl;
+	while (myMapIt != myMap.end()) {
+		std::cout << myMapIt->second << " ";
+		myMapIt++;
+	}
+
+	map2.erase(map2.begin());
+	mapIt = map2.begin();
+	std::cout << " " << std::endl << "Map erase" << std::endl;
+	while (mapIt != map2.end()) {
+		std::cout << mapIt->second << " ";
+		mapIt++;
+	}
+	myMap.erase(myMap.begin());
+	myMapIt = myMap.begin();
+	std::cout << " " << std::endl << "My map erase:" << std::endl;
+	while (myMapIt != myMap.end()) {
+		std::cout << myMapIt->second << " ";
+		myMapIt++;
+	}
+
+	map2.clear();
+	myMap.clear();
+
+	map2.insert(map2.begin(),std::make_pair(6,7));
+	mapIt = map2.begin();
+	std::cout << " " << std::endl << "Map insert:" << std::endl;
+	while (mapIt != map2.end()) {
+		std::cout << mapIt->second << " ";
+		mapIt++;
+	}
+	myMap.insert(myMap.end(), ft::make_pair(6,7));
+	myMapIt = myMap.begin();
+	std::cout << " " << std::endl << "My map insert:" << std::endl;
+	while (myMapIt != myMap.end()) {
+		std::cout << myMapIt->second << " ";
+		myMapIt++;
+	}
+
+	map2.insert(map2.begin(),std::make_pair(8,9));
+	mapIt = map2.begin();
+	std::cout << " " << std::endl << "Map insert:" << std::endl;
+	while (mapIt != map2.end()) {
+		std::cout << mapIt->second << " ";
+		mapIt++;
+	}
+	myMap.insert(myMap.end(), ft::make_pair(8,9));
+	myMapIt = myMap.begin();
+	std::cout << " " << std::endl << "My map insert:" << std::endl;
+	while (myMapIt != myMap.end()) {
+		std::cout <<  myMapIt->second << " ";
+		myMapIt++;
+	}
+
+	map2.insert(map2.begin(),std::make_pair(8,9));
+	mapIt = map2.begin();
+	std::cout << " " << std::endl << "Map re:" << std::endl;
+	while (mapIt != map2.end()) {
+		std::cout << mapIt->second << " ";
+		mapIt++;
+	}
+	myMap.insert(myMap.end(), ft::make_pair(8,9));
+	myMapIt = myMap.begin();
+	std::cout << " " << std::endl << "My map re:" << std::endl;
+	while (myMapIt != myMap.end()) {
+		std::cout << myMapIt->second << " ";
+		myMapIt++;
+	}
+
+	std::cout << std::endl << "Map size: " << map2.size() << std::endl;
+	std::cout << "My map size: " << myMap.size() << std::endl;
+
+	std::map<int, int> map3;
+	map3[1] = 5;
+	map3[2] = 5;
+	map3[3] = 5;
+	std::map<int, int>::iterator mapIt2 = map3.begin();
+	std::cout << "Map2:" << std::endl;
+	while (mapIt2 != map3.end()) {
+		std::cout << mapIt2->second << " ";
+		mapIt2++;
+	}
+	ft::map<int, int> myMap2;
+	myMap2[1] = 9;
+	myMap2[2] = 9;
+	myMap2[3] = 9;
+	ft::map<int, int>::iterator myMapIt2 = myMap2.begin();
+	std::cout << std::endl << "My map2:" << std::endl;
+	while (myMapIt2 != myMap2.end()) {
+		std::cout << myMapIt2->second << " ";
+		myMapIt2++;
+	}
+
+	ft::map<int, int> myMap3;
+	myMap3[1] = 7;
+	myMap3[2] = 7;
+	myMap3[3] = 7;
+	ft::map<int, int>::iterator myMapIt3 = myMap3.begin();
+	std::cout << std::endl << "My map3:" << std::endl;
+	while (myMapIt3 != myMap3.end()) {
+		std::cout << myMapIt3->second << " ";
+		myMapIt3++;
+	}
+	std::cout << std::endl << "swaping" << std::endl;
+	myMap2.swap(myMap3);
+	myMapIt2 = myMap2.begin();
+	std::cout << "My map2:" << std::endl;
+	while (myMapIt2 != myMap2.end()) {
+		std::cout << myMapIt2->second << " ";
+		myMapIt2++;
+	}
+
+	//set
+	std::set<int> st;
+	ft::set<int> mySt;
+	std::cout << std::endl << "Set size: " << st.size() << std::endl;
+	std::cout << "My set size: " << mySt.size() << std::endl;
+
+	for (int i = 1; i < 10; i++) {
+		int r = rand() % 15;
+		st.insert(r);
+		mySt.insert(r);
+	}
+	std::set<int> st2(st.begin(), st.end());
+	ft::set<int> mySt2(mySt.begin(), mySt.end());
+	std::cout << "Set size: " << st2.size() << std::endl;
+	std::cout << "My set size: " << mySt2.size() << std::endl;
+	std::cout << "Set:" << std::endl;
+	for (std::set<int>::iterator it = st.begin(), ite = st.end(); it != ite; it++)
+		std::cout << *it << " " ;
+	std::cout << " " << std::endl;
+	std::cout << "My set:" << std::endl;
+	for (ft::set<int>::iterator myIt = mySt.begin(), ite = mySt.end(); myIt != ite; myIt++)
+		std::cout << *myIt << " " ;
+
+	st.erase((st.begin()));
+	mySt.erase((mySt.begin()));
+	std::cout << " " << std::endl << "Set:" << std::endl;
+	for (std::set<int>::iterator it = st.begin(), ite = st.end(); it != ite; it++)
+		std::cout << *it << " ";
+	std::cout << " " << std::endl << "My set:" << std::endl;
+	for (ft::set<int>::iterator mit = mySt.begin(), ite = mySt.end(); mit != ite; mit++)
+		std::cout << *mit << " ";
+
+	st2.clear();
+	mySt2.clear();
+	for (int i = 100; i < 105; i++) {
+		st2.insert(i);
+		mySt2.insert(i);
+	}
+	std::cout << " " << std::endl << "Set2:" << std::endl;
+	for (std::set<int>::iterator it = st2.begin(), ite = st2.end(); it != ite; it++)
+		std::cout << *it << " ";
+	std::cout << " " << std::endl << "My set2:" << std::endl;
+	for (ft::set<int>::iterator myit = mySt2.begin(), ite = mySt2.end(); myit != ite; myit++)
+		std::cout << *myit << " ";
+	st2.swap(st);
+	mySt2.swap(mySt);
+	std::cout << " " << std::endl << "Set2:" << std::endl;
+	for (std::set<int>::iterator it = st2.begin(), ite = st2.end(); it != ite; it++)
+		std::cout << *it << " ";
+	std::cout << " " << std::endl << "My set2:" << std::endl;
+	for (ft::set<int>::iterator mit = mySt2.begin(), ite = mySt2.end(); mit != ite; mit++)
+		std::cout << *mit << " ";
 	return 0;
 }
--- stack.hpp
+++ stack.hpp
@@ -1,54 +1,59 @@
-#ifndef STACK_HPP
-#define STACK_HPP
+#ifndef STAVK_HPP
+#define STAVK_HPP
+#include "vector.hpp"
 
-#include <vector> //TODO: поменять на "vector.hpp"
-
-namespace ft {
-	template<class T, class Container = std::vector<T> > //TODO: поменять на ft::vector
-	class stack {
+namespace ft
+{
+	template <class T, class Container = ft::vector<T> >
+	class stack
+	{
 	public:
-		typedef typename	Container::value_type value_type;
-		typedef typename	Container::size_type size_type;
-		typedef				Container container_type;
+		typedef T 			value_type;
+		typedef Container 	container_type;
+		typedef size_t 		size_type;
 	protected:
-		Container c;
+		container_type ctnr;
 	public:
-		explicit stack(const Container& = Container()) {}; //TODO: не понятно работает или нет
+		explicit stack(const container_type& _ctnr = container_type()): ctnr(_ctnr){}
+	public:
+		bool empty() const {return (ctnr.empty());}
+		size_type size() const {return (ctnr.size());}
+		value_type& top() {return (ctnr.back());}
+		const value_type& top() const {return (ctnr.back());}
+		void push(const value_type& val) {ctnr.push_back(val);}
+		void pop() {ctnr.pop_back();}
 
-		bool empty() const				{ return c.empty(); }
-		size_type size() const			{ return c.size(); }
-		value_type& top()				{ return c.back(); }
-		const value_type& top() const	{ return c.back(); }
-		void push(const value_type& x)	{ c.push_back(x); }
-		void pop()						{ c.pop_back(); }
+		template <class A, class B>
+		friend bool operator== (const stack<A, B>& lhs, const stack<A, B>& rhs);
 
-		friend bool operator==(const stack &lhs, const stack &rhs) 				{ return lhs.c == rhs.c; };
-		friend bool operator!=(const stack &lhs, const stack &rhs) 				{ return lhs.c != rhs.c; };
-		friend bool operator<=(const stack &lhs, const stack &rhs) 				{ return lhs.c <= rhs.c; };
-		friend bool operator>=(const stack &lhs, const stack &rhs) 				{ return lhs.c >= rhs.c; };
-		friend bool operator<(const stack &lhs, const stack &rhs) 				{ return lhs.c < rhs.c; };
-		friend bool operator>(const stack &lhs, const stack &rhs) 				{ return lhs.c > rhs.c; };
+		template <class A, class B>
+		friend bool operator!= (const stack<A, B>& lhs, const stack<A, B>& rhs);
+
+		template <class A, class B>
+		friend bool operator<  (const stack<A, B>& lhs, const stack<A, B>& rhs);
+
+		template <class A, class B>
+		friend bool operator<= (const stack<A, B>& lhs, const stack<A, B>& rhs);
+
+		template <class A, class B>
+		friend bool operator>  (const stack<A, B>& lhs, const stack<A, B>& rhs);
+
+		template <class A, class B>
+		friend bool operator>= (const stack<A, B>& lhs, const stack<A, B>& rhs);
+
 
 	};
-
 	template <class T, class Container>
-	bool operator==(const stack<T, Container>& x,
-					const stack<T, Container>& y);
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return (lhs.ctnr == rhs.ctnr);}
 	template <class T, class Container>
-	bool operator< (const stack<T, Container>& x,
-					const stack<T, Container>& y);
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return !(lhs == rhs);}
 	template <class T, class Container>
-	bool operator!=(const stack<T, Container>& x,
-					const stack<T, Container>& y);
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return (lhs.ctnr < rhs.ctnr);}
 	template <class T, class Container>
-	bool operator> (const stack<T, Container>& x,
-					const stack<T, Container>& y);
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return !(lhs > rhs);}
 	template <class T, class Container>
-	bool operator>=(const stack<T, Container>& x,
-					const stack<T, Container>& y);
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return (rhs < lhs);}
 	template <class T, class Container>
-	bool operator<=(const stack<T, Container>& x,
-					const stack<T, Container>& y);
-}
-
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs){return !(rhs < lhs);}
+};
 #endif
